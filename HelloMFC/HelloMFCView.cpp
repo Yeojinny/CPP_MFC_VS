869935cccbc28ba2fbb174fcc779debceda6c9ed
Yeojinny <pdg03092@gmail.com>
2021-03-16 17:32:22 +0900

    mfc

---
+++ HelloMFC/HelloMFCView.cpp
@@ -0,0 +1,141 @@
+﻿
+// HelloMFCView.cpp: CHelloMFCView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "HelloMFC.h"
+#endif
+
+#include "HelloMFCDoc.h"
+#include "HelloMFCView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CHelloMFCView
+
+IMPLEMENT_DYNCREATE(CHelloMFCView, CView)
+
+BEGIN_MESSAGE_MAP(CHelloMFCView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_COMMAND(ID_MYMENU_FIRST, &CHelloMFCView::OnMymenuFirst)
+END_MESSAGE_MAP()
+
+// CHelloMFCView 생성/소멸
+
+CHelloMFCView::CHelloMFCView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	char* text = "Hello MFC!";
+	strcpy_s(str, strlen(text) + 1, text);
+	x = y = 0;
+}
+
+CHelloMFCView::~CHelloMFCView()
+{
+}
+
+BOOL CHelloMFCView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CHelloMFCView 그리기
+
+void CHelloMFCView::OnDraw(CDC* pDC)
+{
+	CHelloMFCDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+	pDC->TextOut(x, y, str, strlen(str));
+}
+
+
+// CHelloMFCView 인쇄
+
+BOOL CHelloMFCView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CHelloMFCView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CHelloMFCView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CHelloMFCView 진단
+
+#ifdef _DEBUG
+void CHelloMFCView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CHelloMFCView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CHelloMFCDoc* CHelloMFCView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CHelloMFCDoc)));
+	return (CHelloMFCDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CHelloMFCView 메시지 처리기
+void CHelloMFCView::LButtonWork(UINT nFlags, CPoint point)
+{
+	sprintf_s(str, "%d,%d", point.x, point.y);
+	x = point.x;
+	y = point.y;
+	Invalidate(FALSE);
+}
+
+
+
+void CHelloMFCView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	AfxMessageBox("Mouse Click!", MB_OK);
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CHelloMFCView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	CView::OnLButtonUp(nFlags, point);
+}
+
+
+void CHelloMFCView::OnMymenuFirst()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	AfxMessageBox("OnMymenuFirst Click!", MB_OK);
+}
